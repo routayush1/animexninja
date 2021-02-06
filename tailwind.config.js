497cd0ba7b8eb7ab9556dfdd05b9b190962711dc
Ayush Rout <routayush1@gmail.com>
2021-02-06 15:25:55 +0530

    chaljaa pls

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
