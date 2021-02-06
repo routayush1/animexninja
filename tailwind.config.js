91f64724158a48f9c5c46340661045cf2e3a366c
Ayush Rout <routayush1@gmail.com>
2021-02-06 15:20:09 +0530

    chalja please

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
