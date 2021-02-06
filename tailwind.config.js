d87427cfe967a159481c72d16e1d31e4912b5537
Ayush Rout <routayush1@gmail.com>
2021-02-06 15:09:56 +0530

    fixedpropbly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
