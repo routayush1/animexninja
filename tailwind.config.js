9c9cad5160524ca33e7107203e30f1b2ee2dfda3
Ayush Rout <routayush1@gmail.com>
2021-02-06 15:39:24 +0530

    :-(

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./styles/**/*.css",
-  ],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./styles/**/*.css",
+    ],
+
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: [
+        "bg-background",
+        "text-notactive",
+        "border-notactive",
+        "border-gray-400",
+        "text-gray-400",
+      ],
+    },
+  },
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
