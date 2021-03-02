1da39110a56549e39f6e10618541cd18b4798099
Gaurav Dhiman <gauravdh009@gmail.com>
2021-03-02 22:18:24 +0530

    full on kharcha kia hai

--- components/Details/EpisodeContainer.js
+++ components/Details/EpisodeContainer.js
@@ -47,7 +47,7 @@ const EpisodeContainer = ({ title, id, number, image }) => {
                   />
                 </div>
                 <span className="w-2/3 flex justify-center items-center text-center h-full">
-                  Ep {index}
+                  Ep &nbsp;<span className={"font-semibold"}>{index}</span>
                 </span>
               </EpButton>
             </Link>
