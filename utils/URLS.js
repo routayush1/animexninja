ef2554be47cae253ee5d05706ab8a06b60bd9b4c
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-06-26 01:07:31 +0530

    Update URLS.js

--- utils/URLS.js
+++ utils/URLS.js
@@ -2,7 +2,7 @@ export const URL = {
   POPULAR: process.env.URL + "popular/",
   RECENT: process.env.URL + "recentlyadded/",
   DETAILS: process.env.URL + "details/",
-  EPLINK: process.env.URL + "watching/",
+  EPLINK: process.env.URL + "https://mountainoservo0002.animecdn.com/{ANIMEID}/{EPISODEID}-1080p.mp4",
   GENRES: process.env.URL + "genre/",
   SEARCH: process.env.URL + "search/",
 };
